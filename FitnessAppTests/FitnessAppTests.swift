1d021feb83b2ec7df1882fb1f60c16fb5ab4ae1f
Yordan-It <yordan.it.cor@gmail.com>
2024-10-28 10:49:54 -0300

    Initial Commit

---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by Yordan Iturra Correa on 28-10-24.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
